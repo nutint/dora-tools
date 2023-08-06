948bb6fad9c8f3ead94e91721abdc54fbdb2ba4b
nat <nattha.int@gmail.com>
2023-08-06 15:18:38 +0700

    feat: integrate with dora library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { averageLeadTime } from "@nutint/dora";
+import dayjs from "dayjs";
 
 export default function Home() {
+  const result = averageLeadTime(dayjs(), [dayjs().add(-1, "day")]);
+  console.log({ result });
   return (
     <UserProvider>
       <div>
         Home page
-        <button>Login</button>
+        <a href="/api/v1/auth/login">Login</a>
       </div>
     </UserProvider>
   );
